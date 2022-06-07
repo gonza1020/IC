6d372ffc45d5145683c8f8f130afdeb4a42de091
Matias Acosta <acosta_matias8@hotmail.com>
2022-06-07 20:47:58 -0300

    cambiando texto enlace

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           rel="noopener noreferrer"
           data-testid='learn-link'
         >
-          Learn React
+          Entrar a Twitter
         </a>
       </header>
     </div>
