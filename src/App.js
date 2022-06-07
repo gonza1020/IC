11b51a3fa3e78068d66069fe690bfef6bcd26ecd
Matias Acosta <acosta_matias8@hotmail.com>
2022-06-07 19:52:54 -0300

    viendo algo

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://twitter.com"
+          href="https://facebook.com"
           target="_blank"
           rel="noopener noreferrer"
         >
