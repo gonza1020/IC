8e6dd9c11fc8fa64ddeeeb60c5d4fda896fde780
gonza1020 <gonzalozoloaga44@gmail.com>
2022-06-07 22:35:59 -0300

    Changes

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. Que onda perro
         </p>
         <a
           className="App-link"
           href="https://twitter.com"
           target="_blank"
           rel="noopener noreferrer"
-          data-testid='learn-link'
+          data-testid="learn-link"
         >
           Entrar a Twitter
         </a>
