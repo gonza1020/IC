5208507fcf95528770473426947793a996e705d8
Matias Acosta <acosta_matias8@hotmail.com>
2022-06-07 21:31:53 -0300

    fix develop

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://twitterr.com"
+          href="https://twitter.com"
           target="_blank"
           rel="noopener noreferrer"
           data-testid='learn-link'
