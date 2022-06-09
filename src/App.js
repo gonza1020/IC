5547f4cd66ccbd75438cbece4b8ef1ff2615c60e
Matias Acosta <acosta_matias8@hotmail.com>
2022-06-09 18:17:22 -0300

    PC-11 fallando test

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://facebook.com"
+          href="https://instagram.com"
           target="_blank"
           rel="noopener noreferrer"
           data-testid='learn-link'
